0e3f6ca1fc0f8b43908469099084a00b58cb1285
Jasnoor-singh <142493804+Jasnoor-singh@users.noreply.github.com>
2024-08-23 03:32:03 +0530

    secrets.h

--- secrets.h
+++ secrets.h
@@ -1,4 +1,4 @@
-#define SSID "realme 9 5G Speed Edition"   // Replace with your Wi-Fi SSID
+#define SSID "Jasnoor's Samsung z5 fold"   // Replace with your Wi-Fi SSID
 #define PASSWORD "12345678"      // Replace with your Wi-Fi password
 #define SECRET_CH_ID 2633603          // Replace with your ThingSpeak Channel ID
 #define SECRET_WRITE_APIKEY "CS42SEO9R87T7ICT"    // Replace with your ThingSpeak Write API Key
